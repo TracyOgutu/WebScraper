f970e6b10cb5769d25bb7397a2a2582e96fa6275
Tracy Ogutu <tracyogutu@gmail.com>
2020-03-19 17:18:16 +0300

    filter all java jobs

--- scrape.py
+++ scrape.py
@@ -22,14 +22,14 @@ parseddata=BeautifulSoup(page.content, 'html.parser')
 Finding a specific element by ID
 For easier viewing you can use .prettify()
 '''
-filterbyid=parseddata.find(id='ResultsContainer')
+results=parseddata.find(id='ResultsContainer')
 
-# print(filterbyid.prettify())
+# print(results.prettify())
 
 '''
-Printing the displayed job listings from the filterbyid results 
+Printing the displayed job listings from the results 
 '''
-jobslisted=filterbyid.find_all('section', class_='card-content')
+jobslisted=results.find_all('section', class_='card-content')
 
 for singlejob in jobslisted:
     
@@ -38,15 +38,40 @@ for singlejob in jobslisted:
     company=singlejob.find('div', class_='company')
     location=singlejob.find('div',class_='location')
 
-    # print(singlejob, end='\n'*2)
+    # print( end='\n'*2)
 
     '''
     .text returns only the text content of the HTML elements that the object contains
+    .strip() removes the whitespace
     '''
 
-    print(title.text)
-    print(company.text)
-    print(location.text)
+    if None in (title,company,location):
+        continue
+
+    # print(title.text.strip())
+    # print(company.text.strip())
+    # print(location.text.strip())
+
+'''
+Finding all the java developer openings
+Lambda function looks at the text of each h2 element converts it to lowercase 
+and checks whether the substring is java
+'''
+javadevs=results.find_all('h2', string=lambda text: 'java' in text.lower())
+
+print(f"{len(javadevs)} java jobs found \n")
+
+for java in javadevs:
+    url=java.find('a')['href']
+    print(java.text.strip())
+    print(f"Apply for this job at:{url}\n")
+    
+
+
+
+
+
+
 
 
 
