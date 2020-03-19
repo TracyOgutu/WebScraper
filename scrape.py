e4b8b1898676dcbd8ba4907d66c1325a796da712
Tracy Ogutu <tracyogutu@gmail.com>
2020-03-19 15:40:49 +0300

    add docstrings

--- scrape.py
+++ scrape.py
@@ -2,7 +2,9 @@ import requests
 
 '''
 Performs a http request to the given URL
+The website being scraped in this case serves static HTML content
 '''
+
 URL='https://www.monster.com/jobs/search/?q=Software-Developer&where=Nashville__2C-TN'
 
 page=requests.get(URL)
