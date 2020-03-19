4d6c10b4f0d228eb47e24e6ec9be6a4fe01747c5
Tracy Ogutu <tracyogutu@gmail.com>
2020-03-19 16:31:24 +0300

    add loop to get a single job

--- scrape.py
+++ scrape.py
@@ -1,5 +1,10 @@
 import requests
 
+from bs4 import BeautifulSoup
+'''
+Beautiful soup is a python library for parsing structured data
+'''
+
 '''
 Performs a http request to the given URL
 The website being scraped in this case serves static HTML content
@@ -9,4 +14,39 @@ URL='https://www.monster.com/jobs/search/?q=Software-Developer&where=Nashville__
 
 page=requests.get(URL)
 
-print(page.content)
+parseddata=BeautifulSoup(page.content, 'html.parser')
+
+# print(parseddata)
+
+'''
+Finding a specific element by ID
+For easier viewing you can use .prettify()
+'''
+filterbyid=parseddata.find(id='ResultsContainer')
+
+# print(filterbyid.prettify())
+
+'''
+Printing the displayed job listings from the filterbyid results 
+'''
+jobslisted=filterbyid.find_all('section', class_='card-content')
+
+for singlejob in jobslisted:
+    
+
+    title=singlejob.find('h2', class_='title')
+    company=singlejob.find('div', class_='company')
+    location=singlejob.find('div',class_='location')
+
+    # print(singlejob, end='\n'*2)
+
+    '''
+    .text returns only the text content of the HTML elements that the object contains
+    '''
+
+    print(title.text)
+    print(company.text)
+    print(location.text)
+
+
+
